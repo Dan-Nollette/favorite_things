d6307d90ebf203baf7dbbbf417b206b233dc2eb9
Dan Nollette and Megan Olson <dan@>
2017-09-13 09:04:01 -0700

    test reader and writer methods

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,25 +1,44 @@
 require('item')
 require('rspec')
 
-describe("Item.all") do
-  it("is empty at first") do
-    expect(Item.all()).to(eq([]))
-  end
-end
-
-describe("Item#save") do
-  it("saves an item to the list of items") do
-    item = Item.new("tacos")
-    item.save()
-    expect(Item.all()).to(eq([item]))
-  end
-end
-
-describe("Item.clear") do
-  it("clears all items from the list") do
-    item = Item.new("tacos")
-    item.save()
+describe("Item") do
+  before() do
     Item.clear()
-    expect(Item.all()).to(eq([]))
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Item.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("saves an item to the list of items") do
+      item = Item.new("tacos")
+      item.save()
+      expect(Item.all()).to(eq([item]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears all items from the list") do
+      item = Item.new("tacos")
+      item.save()
+      Item.clear()
+      expect(Item.all()).to(eq([]))
+    end
+  end
+  describe("#name") do
+    it("returns the item's name value") do
+      item = Item.new("tacos")
+      expect(item.name()).to(eq("tacos"))
+    end
+  end
+  describe("#name") do
+    it("changes the item's name value") do
+      item = Item.new("tacos")
+      item.name = "tortilla chips"
+      expect(item.name()).to(eq("tortilla chips"))
+    end
   end
 end
